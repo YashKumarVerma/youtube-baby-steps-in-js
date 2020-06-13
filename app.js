3d66091000f01b500a4db3a75eafcd1cbe6746ab
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-06-13 19:14:48 +0530

    Adding code for example

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Hey rajat !");
+/**
+ * load the express package to a constant
+ */
+const express = require("express");
+
+/**
+ * create an instance of express
+ */
+const app = express();
+
+/**
+ * The port to which the application must listen on
+ */
+const port = 3000;
+
+/**
+ * reply hello when "root" is accessed
+ */
+app.get("/", (incomingRequest, responseToBeSent) => {
+  responseToBeSent.send("Hello World !");
+});
+
+/**
+ * make application listen and respond (basically interact) on port 3000
+ */
+app.listen(port, () =>
+  console.log(`My beast server is running on http://localhost:${port}`)
+);
